cb855943d0a9d94ac128653cac614605131a2ade
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-15 01:02:03 -0300

    Splash screen

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinebox/ui/core/themes/resource.dart';
+import 'package:cinebox/ui/core/widgets/loader_messages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,15 +10,28 @@ class SplashScreen extends ConsumerStatefulWidget {
   ConsumerState<ConsumerStatefulWidget> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends ConsumerState<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> with LoaderAndMessage {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello world"),
-      ),
-      body: Center(
-        child: Image.asset(R.ASSETS_IMAGES_BG_LOGIN_PNG),
+      body: Stack(
+        children: [
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            constraints: BoxConstraints.expand(),
+            color: Colors.black.withAlpha(170),
+          ),
+          Center(
+            child: Image.asset(
+              R.ASSETS_IMAGES_LOGO_PNG,
+            ),
+          ),
+        ],
       ),
     );
   }
