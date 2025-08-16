cd4435da8dae9e042a7c01e002c1edb27e2eab88
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-16 19:46:47 -0300

    Redirect to home on logged in

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinebox/ui/core/themes/resource.dart';
+import 'package:cinebox/ui/core/widgets/loader_messages.dart';
 import 'package:cinebox/ui/login/commands/login_with_google_command.dart';
 import 'package:cinebox/ui/login/login_view_model.dart';
 import 'package:cinebox/ui/login/widgets/sign_in_google_button.dart';
@@ -12,9 +13,20 @@ class LoginScreen extends ConsumerStatefulWidget {
   ConsumerState<ConsumerStatefulWidget> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends ConsumerState<LoginScreen> {
+class _LoginScreenState extends ConsumerState<LoginScreen> with LoaderAndMessage {
   @override
   Widget build(BuildContext context) {
+    ref.listen(loginWithGoogleCommandProvider, (_, state) {
+      state.whenOrNull(
+        data: (_) {
+          Navigator.pushNamedAndRemoveUntil(context, "/home", (routes) => false);
+        },
+        error: (error, st) {
+          showErrorSnackbar('Error on Login');
+        },
+      );
+    });
+
     return Scaffold(
       body: Stack(
         children: [
