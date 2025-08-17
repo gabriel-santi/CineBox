6c6908505a3b1eca26f112c7c865a5582b62187b
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-17 11:10:51 -0300

    Movies by search

---
+++ lib/ui/movies/widgets/movies_by_search.dart
@@ -0,0 +1,23 @@
+import 'package:cinebox/ui/movies/widgets/movies_box.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MoviesBySearch extends ConsumerStatefulWidget {
+  const MoviesBySearch({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MoviesBySearchState();
+}
+
+class _MoviesBySearchState extends ConsumerState<MoviesBySearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 100),
+      child: MoviesBox(
+        title: "Filmes encontrados",
+        vertical: true,
+      ),
+    );
+  }
+}
