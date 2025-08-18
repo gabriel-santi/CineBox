217a3decb07397161989473dd8d194ef19f68fc3
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-18 00:40:15 -0300

    Fix: Displaying movies by genre

--- lib/ui/movies/movies_screen.dart
+++ lib/ui/movies/movies_screen.dart
@@ -2,6 +2,7 @@ import 'package:cinebox/ui/movies/movies_view_model.dart';
 import 'package:cinebox/ui/movies/widgets/genres_box.dart';
 import 'package:cinebox/ui/movies/widgets/movies_appbar.dart';
 import 'package:cinebox/ui/movies/widgets/movies_by_category.dart';
+import 'package:cinebox/ui/movies/widgets/movies_by_gender.dart';
 import 'package:cinebox/ui/movies/widgets/movies_by_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -38,7 +39,7 @@ class _MoviesScreenState extends ConsumerState<MoviesScreen> {
         SliverToBoxAdapter(
           child: switch (moviesViewModel) {
             MoviesViewEnum.byCategory => MoviesByCategory(),
-            MoviesViewEnum.byGenre => MoviesBySearch(),
+            MoviesViewEnum.byGenre => MoviesByGender(),
             MoviesViewEnum.bySearch => MoviesBySearch(),
           },
         ),
